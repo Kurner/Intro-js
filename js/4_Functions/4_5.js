434888ca5c3726af30aae67eb9dd230ab826b6d4
Nicolas <nicolaslibert.pro@gmail.com>
2022-02-17 14:05:12 +0100

    Exercice 4_5 complété

--- js/4_Functions/4_5.js
+++ js/4_Functions/4_5.js
@@ -1,1 +1,17 @@
 const readlineSync = require("readline-sync");
+
+function calcDistance(x1, x2, y1, y2)
+{
+    let y = x2 - x1;
+    let x = y2 - y1;
+        
+    return (Math.sqrt(x * x + y * y));
+}
+
+let x1 = new Number(readlineSync.question('Coordoonnée X du premier point: '));
+let y1 = new Number(readlineSync.question('Coordoonnée Y du premier point: '));
+
+let x2 = new Number(readlineSync.question('Coordoonnée X du second point: '));
+let y2 = new Number(readlineSync.question('Coordoonnée Y du second point: '));
+
+console.log(calcDistance(x1, x2, y1, y2));

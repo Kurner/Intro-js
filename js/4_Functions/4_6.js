948948f3b98b56779931c7fd9da00c6902989599
Nicolas <nicolaslibert.pro@gmail.com>
2022-02-17 14:24:06 +0100

    Exercice 4_6 complété

--- js/4_Functions/4_6.js
+++ js/4_Functions/4_6.js
@@ -1,1 +1,16 @@
 const readlineSync = require("readline-sync");
+
+let a = new Number(readlineSync.question('Nombre à factorialiser ?'));
+
+function factorial(a) {
+    if (a < 0) 
+          return -1;
+    else if (a == 0) 
+        return 1;
+    else {
+        return (a * factorial(a - 1));
+    }
+  }
+ 
+factorial(5);
+console.log(factorial(a));

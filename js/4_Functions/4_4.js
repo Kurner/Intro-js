2d4b2dfb95e452f7a438873c9c8f72368c83fa3c
Nicolas <nicolaslibert.pro@gmail.com>
2022-02-17 13:51:54 +0100

    Exercice 4_4 complété

--- js/4_Functions/4_4.js
+++ js/4_Functions/4_4.js
@@ -1,1 +1,44 @@
 const readlineSync = require("readline-sync");
+
+let $n = new Number(readlineSync.question('Un nombre ?'));
+let arr = [];
+
+function min(arr)
+{
+    return minArr = Math.min.apply(Math, arr);        
+}
+
+function max(arr)
+{
+    return maxArr = Math.max.apply(Math, arr);
+}
+
+function average(arr)
+{
+    var somme = 0;
+    for (let elem of arr)
+    {
+        somme += elem
+    }
+    return avg = (somme/arr.length);
+}
+
+function multiRand($n)
+{
+    while($n > 0)
+    {
+        function rand10()
+        {
+            return Math.floor(Math.random() * 10);
+        }
+
+        arr.push(rand10());
+        $n--;
+    }
+
+    let result = [average(arr), min(arr), max(arr)];
+    return result;
+}
+
+multiRand($n);
+console.log(multiRand());

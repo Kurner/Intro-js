2c226e40bdd0c1476ff41814879605cbc204f672
Nicolas <nicolaslibert.pro@gmail.com>
2022-02-18 15:05:23 +0100

    Revert "Correction de l'exercice 6_1"
    This reverts commit dd03008bf30eab20305503ece832c4f7f59a84db.

--- js/6_Classes/6_1.js
+++ js/6_Classes/6_1.js
@@ -1,5 +1,26 @@
 const readlineSync = require("readline-sync");
 
+// class Circle {
+//     constructor(xPos, yPos, radius){
+//         this.xPos   = xPos;
+//         this.yPos   = yPos;
+//         this.radius = radius
+//     }
+//     get surface(){
+//         let surface = (Math.pow(this.radius, 2)) * Math.PI;
+
+//         return surface.toFixed(2);
+//     }
+//     move(xOffset, yOffset){
+//         this.xPos = xOffset;
+//         this.yPos = yOffset;
+//     }
+// }
+
+// let bubble = new Circle(1, 5, 10);
+
+// console.log(bubble.surface)
+
 class Circle
 {
     constructor(xPos, yPos, radius)

13d5ba71663d5b4e0474e2c65eeb1d91f667e4bf
Nicolas <nicolaslibert.pro@gmail.com>
2022-02-21 16:25:47 +0100

    Exercice 7_3 complété

--- js/7_Extras/7_3.js
+++ js/7_Extras/7_3.js
@@ -1,1 +1,24 @@
 const readlineSync = require("readline-sync");
+
+
+function divisors()
+{
+    let n = new Number(readlineSync.question('Un chiffre positif : '));
+
+    while(n <= 0)
+    {
+        n = new Number(readlineSync.question('ON A DIT POSITIF : '));
+    }
+
+    let divisors = [];
+    for(let i = 2; i<n; i++)
+    {
+        if(n % i === 0)
+        {
+            divisors.push(i);
+        }
+    }
+    return divisors.length > 1 ? divisors : `${n} is a prime number`
+}
+
+console.log(divisors())

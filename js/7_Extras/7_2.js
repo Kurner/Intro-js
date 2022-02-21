fe885b102ae919a79355ec18cbacaadb55f26879
Nicolas <nicolaslibert.pro@gmail.com>
2022-02-21 15:43:50 +0100

    Exercice 7_2. complété

--- js/7_Extras/7_2.js
+++ js/7_Extras/7_2.js
@@ -1,1 +1,24 @@
 const readlineSync = require("readline-sync");
+
+let n = new Number(readlineSync.question('Donne un chiffre : '))
+let nextTerm;
+let n1 = 0;
+let n2 = 1;
+
+function fibonacci(){
+   
+    nextTerm = n1 + n2;
+
+    while(n >= nextTerm)
+    {
+        console.log(nextTerm);
+
+        n1 = n2;
+        n2 = nextTerm;
+        nextTerm = n1 + n2;
+    }
+
+    return nextTerm;
+}
+
+console.log(fibonacci());

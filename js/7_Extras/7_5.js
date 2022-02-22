82fc83b1da2e9086beffc940982b453857403079
Nicolas <nicolaslibert.pro@gmail.com>
2022-02-22 15:10:33 +0100

    Exercice 7_5 complété

--- js/7_Extras/7_5.js
+++ js/7_Extras/7_5.js
@@ -1,1 +1,30 @@
 const readlineSync = require("readline-sync");
+
+
+let arr = [];
+
+function arraySort(arr)
+{
+    let fini = false;
+    while(!fini)
+    {   
+        fini = true;
+        for(let i = 0; i < arr.length; i++)
+        {
+            let temp = 0;
+            if(arr[i] > arr[i+1])
+            {
+                temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                fini = false;
+            }
+
+        }      
+    }      
+    return arr;
+}
+
+
+let numberArray = [13,5,7,23,1,0];
+console.log(arraySort(numberArray));
